1b077e1bf9b26c1646bfefac7926f14f7363c0ce
Oliver Pinel <oliver.debankle@gmail.com>
2024-03-05 20:01:50 +1000

    Added comments and cleaned up styling

--- Tower of Hanoi/main.py
+++ Tower of Hanoi/main.py
@@ -1,14 +1,33 @@
+def hanoi(n,start,end,temp):
+    """
+    Solves the Tower of Hanoi problem recursively
 
+    Parameters:
+        n (int): Number of disks.
+        start (str): The starting peg.
+        end (str): The destination peg.
+        temp (str): The temporary peg.
 
-def Hanoi(n,start,end,temp):
+    Returns:
+        None
+    """
     if n == 1:
+        # Base case of recursion: move last piece disk on a peg
+        # to the destination peg
         print("Move disk 1 from " + start + " to " + end)
     else:
-        Hanoi(n-1,start,temp,end)
+        # Clear the start peg so the last disk can be moved to the
+        # bottom of the destination peg
+        hanoi(n-1,start,temp,end)
+        # Move the last disk from the start to the end
         print("Move disk " + str(n) + " from " + start + " to " + end)
-        Hanoi(n-1,temp,end,start)
+        # Move n-1 disks from temp to end using start as auxiliary.
+        hanoi(n-1,temp,end,start)
 
 if __name__ == "__main__":
-    Hanoi(3,"A","B","C")
+    print("Testing the function with 3 disks")
+    hanoi(3,"A","B","C")
     print()
-    Hanoi(5,"A","B","C")
+
+    print("Testing the function with 5 disks")
+    hanoi(5,"A","B","C")
